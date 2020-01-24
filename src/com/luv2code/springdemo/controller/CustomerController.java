daac532a74a5f468e1c84b4fc991609d79564b9b
t822660 <t822660@spark.co.nz>
2020-01-24 14:09:19 +1300

    updated

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,15 +1,33 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	//need to inject the customer dao
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomer(Model theModel) {
+		
+		//get customers from the dao
+		List<Customer> theCustomers=customerDAO.getCustomers();
+		
+		System.out.println("in controller"+theCustomers);
+		
+		//add customer to the model
+		theModel.addAttribute("customers", theCustomers);
 		
 		return "list-customers";
 	}
---
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,40 @@
+package com.luv2code.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Autowired
+	private SessionFactory factory;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+
+		//get current hibernate session
+		Session session=factory.getCurrentSession();
+		
+		//create a query
+		Query<Customer> theQuery=session.createQuery("from Customer", Customer.class);
+		
+		//execute query and get result list
+		List<Customer> customers=theQuery.getResultList();
+		
+		System.out.println("in DAO: "+customers);
+		
+		//return the results
+		return customers;
+		
+	}
+
+}
